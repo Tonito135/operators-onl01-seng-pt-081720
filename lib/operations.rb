119eb1d602020708909b539140a47395a7f49d45
Anthony Hernandez <anthonyh2020@gmail.com>
2020-08-27 16:19:26 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,14 @@
 def unsafe?(speed)
-
+  if speed > 60 || speed < 40
+    return "unsafe"
+  else speed
+    return "safe"
 end
 
 
 
 def not_safe?(speed)
-	
+	60 < speed < 40 ? "safe" : "not safe"
 end
 	
 
